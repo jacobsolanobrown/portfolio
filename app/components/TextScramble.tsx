305a07ec3cf6ccd2310f9f715f5d4bcfc4cdde4e
jacobsolanobrown <gilbert_solano@brown.edu>
2026-02-18 08:39:53 -0800

    started scramble functionalitym

---
+++ app/components/TextScramble.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+'use client';
+
+
+interface TextScrambleProps {
+  // Add your props here
+  text: string
+}
+
+const TextScramble: React.FC<TextScrambleProps> = (props) => {
+
+  const [displayedText, setDisplayedText] = React.useState('');
+
+  useEffect(() => { 
+
+  return (
+    <div>
+      {/* Add your content here */}
+
+    </div>
+  );
+};
+
+export default TextScramble;
